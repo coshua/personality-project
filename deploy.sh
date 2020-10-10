a7d34e8c2a1a535c5e18982b846c5ad18779a3f9
Dongju <bitlez3470@gmail.com>
2020-10-10 18:08:07 +0900

    travis

---
+++ deploy.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+AWS_S3_REGION="ap-northeast-2"
+STAGING_BRANCH="test"
+PRODUCTION_BRANCH="production"
+
+# Determine the environment to deploy to based on the branch
+# From that, we'll know:
+#   - the S3 bucket to sync assets with
+#   - the CloudFront distribution for which to create an invalidation
+NODE_ENV=''
+CLOUDFRONT_DIST_ID=''
+S3_BUCKET=''
+if [[ $TRAVIS_BRANCH == $STAGING_BRANCH ]]; then
+  NODE_ENV="test"
+  CLOUDFRONT_DIST_ID=$CLOUDFRONT_DIST_ID_STAGING
+  S3_BUCKET="personality-project-$NODE_ENV"
+  yarn build
+elif [[ $TRAVIS_BRANCH == $PRODUCTION_BRANCH ]]; then
+  NODE_ENV="production"
+  CLOUDFRONT_DIST_ID=$CLOUDFRONT_DIST_ID_PRODUCTION
+  S3_BUCKET="personality-project"
+  yarn build
+else
+  # Don't want to deploy if it's not one of the above branches
+  echo "Not deploying"
+  exit
+fi
+
+# Build the name of the S3 bucket we want to deploy to
+echo "Deploying to the $S3_BUCKET bucket"
+
+# Install the AWS CLI so we can publish to S3
+pip install awscli --upgrade --user
+
+# Sync our build folder with our S3 bucket
+# --acl public-read says deploy the files with public read access
+# --delete says to delete files in the bucket that aren't present in the build folder
+#   this ensures that old assets built with webpack with hashed names get deleted
+#   when a new build of the app is made and the assets get new hash names
+aws s3 sync public/ "s3://$S3_BUCKET" --acl public-read --delete
+
+# Force-invalidate the now-outdated assets rather than waiting for them to expire
+# Make sure you have the CLOUDFRONT_DIST_ID_* env variables defined for this to work
+aws cloudfront create-invalidation \
+  --distribution-id $CLOUDFRONT_DIST_ID \
+  --paths /*
